6b2c012b178df9353a828942d7398ccd06492569
Xinyi Le (FESCO Adecco Human Resources) <v-lexinyi@microsoft.com>
2024-05-20 11:46:30 +0800

    order and divisibility

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -10,6 +10,8 @@ variable (a b c d : ℝ)
 #check (min_le_right a b : min a b ≤ b)
 #check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
 
+-- 与之对应的max
+
 example : min a b = min b a := by
   apply le_antisymm
   · show min a b ≤ min b a
@@ -28,28 +30,86 @@ example : min a b = min b a := by
     apply min_le_right
     apply min_le_left
   apply le_antisymm
-  apply h
-  apply h
+  apply h -- here it is apply h to a and b
+  apply h -- here it is apply h to b and a
 
 example : min a b = min b a := by
   apply le_antisymm
-  repeat
+  repeat -- apply a tactic as many times as it can
     apply le_min
     apply min_le_right
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
-example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
+
+example : min (min a b) c = min a (min b c) := by -- so hard!!!
+  apply le_antisymm
+  · show min (min a b) c ≤ min a (min b c)
+    apply le_min
+    · apply le_trans
+      apply min_le_left
+      apply min_le_left
+    apply le_min
+    · apply le_trans
+      apply min_le_left
+      apply min_le_right
+    apply min_le_right
+  · show  min a (min b c) ≤ min (min a b) c
+    apply le_min
+    · apply le_min
+      apply min_le_left
+      apply le_trans
+      apply min_le_right
+      apply min_le_left
+    apply le_trans
+    apply min_le_right
+    apply min_le_right
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  · show min a b + c ≤ a + c
+    apply add_le_add_right
+    apply min_le_left
+  · show min a b + c ≤ b + c
+    apply add_le_add_right
+    apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · show min a b + c ≤ min (a + c) (b + c)
+    apply aux
+  · show min (a + c) (b + c) ≤ min a b + c
+    have h : min (a + c) (b + c) = min (a + c) (b + c) - c + c := by rw [sub_add_cancel]
+    rw [h]
+    apply add_le_add_right
+    rw [sub_eq_add_neg]
+    apply le_trans
+    apply aux
+    rw [add_neg_cancel_right]
+    rw [add_neg_cancel_right]
+
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
 example : |a| - |b| ≤ |a - b| :=
-  sorry
+  calc
+    |a| - |b| ≤ |a - b + b| - |b| := by rw [sub_add_cancel]
+    _ ≤ |a - b| + |b| - |b| := by
+      apply sub_le_sub_right
+      apply abs_add
+    _ ≤ |a - b| := by rw [add_sub_cancel_right]
+
+-- alternatively
+example : |a| - |b| ≤ |a - b| := by
+  have h := abs_add (a - b) b
+  rw [sub_add_cancel] at h
+  linarith
 end
 
 section
@@ -66,7 +126,15 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  · apply dvd_add
+    rw [mul_comm]
+    apply dvd_mul_of_dvd_left
+    apply dvd_mul_right
+    apply dvd_mul_left
+  · apply dvd_trans
+    apply h
+    apply dvd_mul_left
 end
 
 section
@@ -78,7 +146,9 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply dvd_antisymm -- use the anti-symmetry of divisibility
+  repeat'
+    apply dvd_gcd
+    apply gcd_dvd_right
+    apply gcd_dvd_left
 end
-
-
