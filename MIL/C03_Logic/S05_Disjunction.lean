5bbffa39c8476ee0c8cdb54ccf3e807d3727a78d
Xinyi Le (FESCO Adecco Human Resources) <v-lexinyi@microsoft.com>
2024-05-21 14:47:25 +0800

    3.5

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -59,19 +59,61 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg]
+    exact h
+  · rw [abs_of_neg]
+    linarith
+    exact h
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg]
+    linarith
+    exact h
+  · rw [abs_of_neg]
+    exact h
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  · rw [abs_of_nonneg]
+    linarith [le_abs_self x, le_abs_self y]
+    exact h
+  · rw [abs_of_neg]
+    linarith [neg_le_abs_self x, neg_le_abs_self y]
+    exact h
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+constructor
+rcases le_or_gt 0 y with h | h
+· rw [abs_of_nonneg]
+  intro h'
+  left
+  exact h'
+  exact h
+· rw [abs_of_neg]
+  intro h''
+  right
+  exact h''
+  exact h
+
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  constructor
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg]
+    intro h'
+    constructor
+    · linarith
+    · exact h'
+    exact h
+  · rw [abs_of_neg]
+    intro h''
+    constructor
+    · linarith
+    · linarith
+    exact h
+
 
 end MyAbs
 
@@ -92,38 +134,81 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, rfl | rfl⟩ <;> linarith [sq_nonneg x, sq_nonneg y]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have : x ^ 2 - 1 = 0 := by linarith
+  have : x ^ 2 - 1 = (x + 1) * (x - 1) := by linarith
+  have : (x + 1) * (x - 1) = 0 := by linarith
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  · right
+    linarith
+  · left
+    linarith
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have : x ^ 2 - y ^ 2 = 0 := by linarith
+  have : (x + y) * (x - y) = 0 := by linarith
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  · right
+    linarith
+  · left
+    linarith
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by
+    rw [h]
+    ring
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h₀ | h₀
+  · right
+    exact (neg_eq_of_add_eq_zero_left h₀).symm
+  · left
+    exact eq_of_sub_eq_zero h₀
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by
+    rw [h]
+    ring
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h₀ | h₀
+  · right
+    exact (neg_eq_of_add_eq_zero_left h₀).symm
+  · left
+    exact eq_of_sub_eq_zero h₀
 
 end
 
 example (P : Prop) : ¬¬P → P := by
   intro h
-  cases em P
+  cases em P -- case work: either P is true or ¬P is true
   · assumption
   . contradiction
 
 example (P : Prop) : ¬¬P → P := by
   intro h
-  by_cases h' : P
+  by_cases h' : P -- same as above
   · assumption
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  · intro h
+    by_cases h' : P
+    right
+    exact h h'
+    left
+    exact h'
+  rintro (h | h)
+  · intro h'
+    exact absurd h' h
+  · intro h''
+    exact h
